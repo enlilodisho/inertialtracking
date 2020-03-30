2e7197e56d4aab96d0947171f75e35311d0127b6
Enlil Odisho <egodisho@ucsd.edu>
2020-03-30 05:54:41 +0100

    read data & status from fifo

--- src/InertialTracking.cpp
+++ src/InertialTracking.cpp
@@ -19,43 +19,53 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #include <iostream>
+#include <chrono>
 #include "../include/LSM9DS1-RPI/LSM9DS1.hpp"
 
 LSM9DS1 lsm;
+
+using namespace std::chrono;
 
 /**
  * Main function.
  */
 int main(int argc, char * argv[]) {
-    std::cout << "Hello world!" << std::endl;
-    //lsm.set_ag_odr(AG_ODR::AG_ODR_952); // too fast for raspberry pi
-    lsm.set_ag_odr(AG_ODR::AG_ODR_476);
+    std::cout << "Inertial Tracking starting..." << std::endl;
+
+    lsm.set_ag_odr(AG_ODR::AG_ODR_119); // settings faster than 238Hz are too fast for rpi
     lsm.set_g_scale(G_SCALE::G_SCALE_2000);
-    lsm.set_drdy_enable_bit(true);
-    //std::cout << "Status: " << lsm.get_status_reg() << std::endl;
+
+    //lsm.set_drdy_enable_bit(true); // Enable data ready status registers
+    
+    // Turn on FIFO mode
+    lsm.set_fifo_enable_bit(true); // Enable FIFO feature
+    lsm.set_fifo_mode(FIFO_MODE_BYPASS); // reset FIFO buffer contents
+    lsm.set_fifo_mode(FIFO_MODE_ON); // Using regular FIFO mode. If buffer gets full, output will stop until reset!
+
+    static high_resolution_clock::time_point last = high_resolution_clock::now();
+    high_resolution_clock::time_point now;
+
     int i = 0;
-    while (i < 10000) {
-        unsigned char status = lsm.get_status_reg();
-        /*std::cout << "ACC: " << ((lsm.is_acc_available(status) == true) ? "true" : "false") << std::endl;
-        std::cout << "GYRO: " << ((lsm.is_gyro_available(status) == true) ? "true" : "false") << std::endl;
-        std::cout << "TEMP: " << ((lsm.is_temp_available(status) == true) ? "true" : "false") << std::endl;
-        std::cout << std::endl;*/
+    while (i < 1000) {
+        now = high_resolution_clock::now();
 
-        bool gyroAvail = lsm.is_gyro_available(status);
-        if (gyroAvail == true) {
+        unsigned char fifoStatus = lsm.get_fifo_status();
+        uint8_t numFifoUnread = lsm.get_num_fifo_unread(fifoStatus);
+        printf("[%d] Unread: %d, Overrun: %s, Threshold: %s", i,
+                (int)numFifoUnread,
+                ((lsm.did_fifo_overrun(fifoStatus) == true) ? "yes" : "no "),
+                ((lsm.is_fifo_threshold_reached(fifoStatus) == true) ? "yes": "no "));
+
+        if (numFifoUnread > 0) {
+            duration<double, std::nano> dt = duration_cast<duration<double, std::nano>>(now - last);
+
             struct SensorData gyro = lsm.get_angular_rate();
-            printf("gyro: %d, %d, %d\n", gyro.x, gyro.y, gyro.z);
-        } else {
-            //std::cout << "gyro: WAIT\n";
+            struct SensorData acc = lsm.get_linear_acc();
+            printf(" ; acc:%d,%d,%d, gyro:%d,%d,%d ; [dt:%fus]", 
+                    acc.x, acc.y, acc.z, gyro.x, gyro.y, gyro.z, dt.count());
         }
 
-        bool accAvail = lsm.is_acc_available(status);
-        if (accAvail == true) {
-            struct SensorData acc = lsm.get_linear_acc();
-            printf("acc: %d, %d, %d\n", acc.x, acc.y, acc.z);
-        } else {
-            //std::cout << "acc: WAIT\n";
-        }
+        std::cout << std::endl;
 
         i++;
     }
